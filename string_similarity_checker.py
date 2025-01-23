89d47cbcc60686a3e2b7e36de21de198b119d98e
ufunc <108303230+ufunc@users.noreply.github.com>
2025-01-23 23:27:39 +0100

    Create string_similarity_checker.py
    Added string similarity checker

---
+++ string_similarity_checker.py
@@ -0,0 +1,9 @@
+print('Welcome to string similarity checker!')
+
+string_1 = input('Enter first string\n: ')
+string_2 = input('Enter the second string\n: ')
+
+if string_1 == string_2:
+  print('They are similar!')
+else:
+  print('They are not similar!')
