302eed219c5aced06b0f3c762c92e7a8622fb101
ufunc <108303230+ufunc@users.noreply.github.com>
2025-01-28 21:21:44 +0100

    Update even_number_checker.py
    Improve code

--- even_number_checker.py
+++ even_number_checker.py
@@ -1,7 +1,14 @@
-num = int(input('Enter the number: '))
+while True:
 
-if num % 2 == 0:
-  print('The numbers is an even number!')
-else:
-  print('It is an odd number!')
+    num = int(input('Enter the number: '))
+
+    if num % 2 == 0:
+      print('This is an even number!')
+    else:
+      print('This is an odd number!')
+   
+    print('Do your want to check another number? (yes/no)')
+       if not input('> ').lower().startswith('y'):
+          break
+print('Goodbye!')
   
